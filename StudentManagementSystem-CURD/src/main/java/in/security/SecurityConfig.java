c33d5eb3428ac7eab41f7a9fe982ab46b7242e90
Pawan Pawar <pawanpawar24@gmail.com>
2025-10-04 01:17:57 +0530

    Update SecurityConfig.java

--- StudentManagementSystem-CURD/src/main/java/in/security/SecurityConfig.java
+++ StudentManagementSystem-CURD/src/main/java/in/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -18,9 +19,12 @@ public class SecurityConfig {
             .authorizeHttpRequests(auth -> auth
                 .anyRequest().authenticated()
             )
-           // .formLogin(withDefaults())
+            .sessionManagement(session -> 
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            )
+            //.formLogin(withDefaults())  // enable if needed
             .httpBasic(withDefaults());
-        
+
         return http.build();
     }
 }
